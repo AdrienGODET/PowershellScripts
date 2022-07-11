686cf9bfa3a56be1aa7e735cc5ecdb9d7db3c7fa
Adrien GODET <58424329+AdrienGODET@users.noreply.github.com>
2022-07-11 18:22:09 +0200

    Create New-NatVSwitch.ps1
    First Release

---
+++ New-NatVSwitch.ps1
@@ -0,0 +1,4 @@
+# Create Nat Adapter Hyper-v 
+New-VMSwitch -Name "NAT" -SwitchType Internal
+New-NetIPAddress -IPAddress 192.168.200.1 -PrefixLenght 24 -InterfaceAlias "vEthernet (NAT)"
+New-NetNat -Name NAT -InternalIPInterfaceAddressPrefix 192.168.200.0/24
